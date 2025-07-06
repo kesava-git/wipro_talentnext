7948ace8b21c586230eb9f88f6e887094bee279b
kesav@kesava <kesava1045@gmail.com>
2025-07-06 21:16:41 +0530

    Completed All Java Fundamentals Questions

--- JavaFundamentals/src/Java_Fundamentals/Question_25.java
+++ JavaFundamentals/src/Java_Fundamentals/Question_25.java
@@ -3,21 +3,32 @@ package Java_Fundamentals;
 public class Question_25 {
 
 	public static void main(String[] args) {
-		int[] arr= {21,9,4,5,6,7,10,9,20,20,20,20,15};
-		int max1=Integer.MIN_VALUE;
-		int max2=Integer.MIN_VALUE;
-		int min1=0;
-		int min2=Integer.MAX_VALUE;
-		for(int i=0;i<arr.length;i++) {
-			if(max1<arr[i]) {
-				max2=max1;
-				max1=arr[i];				
-			}
-			else if(max2<arr[i]&&max1>arr[i]) {
-				max2=arr[i];
+		int[] arr = { 12, 45, 2, 67, 89, 1, 34, 78, 89, 2};
+		int smallest1 = Integer.MAX_VALUE, smallest2 = Integer.MAX_VALUE;
+		int largest1 = Integer.MIN_VALUE, largest2 = Integer.MIN_VALUE;
+		for (int num : arr) {
+			if (num < smallest1) {
+				smallest2 = smallest1;
+				smallest1 = num;
+			} else if (num < smallest2 && num != smallest1) {
+				smallest2 = num;
 			}
 		}
-		System.out.println(max1);
+		for (int num : arr) {
+			if (num > largest1) {
+				largest2 = largest1;
+				largest1 = num;
+			} else if (num > largest2 && num != largest1) {
+				largest2 = num;
+			}
+		}
+		if (smallest2 == Integer.MAX_VALUE)
+			smallest2 = smallest1;
+		if (largest2 == Integer.MIN_VALUE)
+			largest2 = largest1;
+
+		System.out.println("Smallest two numbers: " + smallest1 + ", " + smallest2);
+		System.out.println("Largest two numbers: " + largest1 + ", " + largest2);
 	}
 
 }
--- JavaFundamentals/src/Java_Fundamentals/Question_31.java
+++ JavaFundamentals/src/Java_Fundamentals/Question_31.java
@@ -1,5 +1,4 @@
 package Java_Fundamentals;
-
 public class Question_31 {
 	public static void main(String[] args) {
 		int[] arr= {1,4,5,1,4};
@@ -16,3 +15,4 @@ public class Question_31 {
 		}
 	}
 }
+
