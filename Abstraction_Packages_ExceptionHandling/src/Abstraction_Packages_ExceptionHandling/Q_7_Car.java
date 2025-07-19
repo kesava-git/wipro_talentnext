9130d6f688e533fc9eb9c968965d41b19b8f424e
kesav@kesava <kesava1045@gmail.com>
2025-07-19 18:45:14 +0530

    19--July--Abstraction_Packages_ExceptionHandling

---
+++ Abstraction_Packages_ExceptionHandling/src/Abstraction_Packages_ExceptionHandling/Q_7_Car.java
@@ -0,0 +1,14 @@
+package Abstraction_Packages_ExceptionHandling;
+
+public class Q_7_Car implements Vehicle,FourWheeler {
+	public static void main(String[] args) {
+		Q_7_Car car=new Q_7_Car();
+		car.message();
+	}
+
+	@Override
+	public void message() {
+		Vehicle.super.message();
+		FourWheeler.super.message();
+	}
+}
---
+++ Abstraction_Packages_ExceptionHandling/src/Abstraction_Packages_ExceptionHandling/Question_10.java
@@ -0,0 +1,32 @@
+package Abstraction_Packages_ExceptionHandling;
+
+import java.util.Scanner;
+
+public class Question_10 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int[] arr=new int[5];
+		int sum=0;
+		try {
+			for(int i=0;i<arr.length;i++) {
+				arr[i]=Integer.parseInt(sc.nextLine());
+				sum=sum+arr[i];
+			}
+			double average=sum/5.0;
+			System.out.println("Sum = "+sum);
+			System.out.println("Average = "+average);
+		}
+		catch (NumberFormatException e) {
+            System.out.println("Error: Please enter only integer values. " + e);
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: " + e);
+		}
+		catch (ArithmeticException e) {
+            System.out.println("Error during arithmetic operation: " + e);
+        } catch (Exception e) {
+            System.out.println("Some unexpected error occurred: " + e);
+        }
+	}
+		
+}
